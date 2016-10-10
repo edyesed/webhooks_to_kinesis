31925b915559f10fd944c644109953fbabde4f6c
Ed Anderson <ed@realself.com>
2016-10-09 17:31:11 -0700

    Building damn near everything

--- bin/build_pieces.sh
+++ bin/build_pieces.sh
@@ -4,6 +4,9 @@
 # OHAI, you're reading this? 
 # set an  ENV variable named 
 # PROFILE to run as not-default
+## 
+## Everything is in us-west-2. change that if you need to
+## 
 if [[ -z $PROFILE ]]; then
   IAM_PROFILE=""
 else
@@ -15,11 +18,12 @@ which aws >/dev/null || { echo "Install aws cli"; exit 1; }
 AWS=`aws iam create-role ${IAM_PROFILE} --role-name api_kinesis_post --assume-role-policy-document '{ "Version": "2012-10-17", "Statement": [ { "Sid": "", "Effect": "Allow", "Principal": { "Service": "apigateway.amazonaws.com" }, "Action": "sts:AssumeRole" } ] }' 2>&1`
 EXIT=$?
 if [[ ${EXIT} -eq 0 ]]; then
-  :
+  # XXX come back and parse this shit
+  ROLEARN=`echo ${AWS} | python -c 'import sys, json; print json.load(sys    .stdin)["arn"];'`
 else 
   if echo ${AWS} | grep 'already exists\.$' >/dev/null; then
     # null operator because it already exists
-    :
+    ROLEARN=`aws iam get-role ${IAM_PROFILE}  --role-name api_kinesis_post | grep Arn | awk '{print $(NF)}' | sed -e 's/"//g'`
   else
     echo "${AWS}"
     exit ${EXIT}
@@ -119,22 +123,22 @@ AWS=`aws apigateway get-resources ${IAM_PROFILE} --rest-api-id ${OURAPI} 2>&1`
 EXIT=$?
 if [[ ${EXIT} -eq 0 ]]; then
   PARENTID=`echo ${AWS} | python -c 'import sys, json; ins=(json.load(sys.stdin)["items"]); apis=[ d["'"id"'"] for d in ins if d["'"path"'"] == "/"]; print apis[0];'`
-  STREAMSID=`echo ${AWS} | python -c 'import sys, json; ins=(json.load(sys.stdin)["items"]); apis=[ d["'"id"'"] for d in ins if d["'"path"'"] == "/streams"]; print apis[0];' 2>/dev/null`
+  STREAMSID=`echo ${AWS} | python -c 'import sys, json; ins=(json.load(sys.stdin)["items"]); apis=[ d["'"id"'"] for d in ins if d["'"path"'"] == "/islandstreams"]; print apis[0];' 2>/dev/null`
 else 
   echo "${AWS}"
   exit ${EXIT}
 fi
 
-# Create the /streams path 
+# Create the /islandstreams path 
 if [[ -z ${STREAMSID} ]]; then
-  AWS=`aws apigateway create-resource ${IAM_PROFILE} --rest-api-id ${OURAPI} --parent-id ${PARENTID} --path-part streams 2>&1`
+  AWS=`aws apigateway create-resource ${IAM_PROFILE} --rest-api-id ${OURAPI} --parent-id ${PARENTID} --path-part islandstreams 2>&1`
   EXIT=$?
   if [[ ${EXIT} -eq 0 ]]; then
-    :
+    # null operator because it already exists
+    STREAMSID=`echo ${AWS} | python -c 'import sys, json; ins=json.load(sys.stdin)["id"]; print ins;' 2>/dev/null`
   else 
     if echo ${AWS} | grep 'Another resource with the same parent already has this name' >/dev/null; then
-      # null operator because it already exists
-      STREAMSID=`echo ${AWS} | python -c 'import sys, json; ins=(json.load(sys.stdin)["items"]); apis=[ d["'"id"'"] for d in ins if d["'"path"'"] == "/streams"]; print apis[0];' 2>/dev/null`
+      :
     else
       echo "${AWS}"
       exit ${EXIT}
@@ -143,6 +147,108 @@ if [[ -z ${STREAMSID} ]]; then
 fi
 
 if [[ -z ${STREAMSID} ]]; then
-  echo "/streams path in the api gateway was not found and was not created. ur screwed."
+  echo "/islandstreams path in the api gateway was not found and was not created. ur screwed."
   exit 1
 fi
+if [[ -z ${ROLEARN} ]]; then
+  echo "didn't make an iam role. cannot continue"
+  exit 1
+fi
+
+
+
+### Some things have to be done by raw input
+INPUTFILE="/var/tmp/put_method.json.$$"
+cat <<EOT > ${INPUTFILE}
+{
+    "ApiKeyRequired": false,
+    "AuthorizationType": "NONE",
+    "HttpMethod": "POST",
+    "MethodIntegration": {
+        "integrationResponses": {
+            "200": {
+                "responseTemplates": {
+                    "application/json": null
+                },
+                "statusCode": "200"
+            }
+        },
+        "cacheKeyParameters": [],
+        "requestParameters": {
+            "integration.request.header.ContentType": "'application/x-amz-json-1.1'"
+        },
+        "uri": "arn:aws:apigateway:us-west-2:kinesis:action/PutRecord",
+        "httpMethod": "POST",
+        "requestTemplates": {
+            "application/json": "{ \"StreamName\": \"webhooks\",\n  \"Data\": \"$util.base64Encode($input.path('$'))\",\n  \"PartitionKey\": \"shardId-000000000000\" }"
+        },
+        "cacheNamespace": "5eqljb",
+        "credentials": "arn:aws:iam::*:role/apigateway_kinesis",
+        "type": "AWS"
+    },
+    "requestParameters": {},
+    "methodResponses": {
+        "200": {
+            "responseModels": {
+                "application/json": "Empty"
+            },
+            "statusCode": "200"
+        }
+    }
+}
+EOT
+
+# add the put method on the resource
+AWS=`aws apigateway put-method ${IAM_PROFILE} --rest-api-id ${OURAPI} --resource-id ${STREAMSID} --http-method 'POST' --authorization-type 'NONE' --no-api-key-required --request-parameters "method.request.header.custom-header=false" --region us-west-2 2>&1`
+EXIT=$?
+if [[ ${EXIT} -eq 0 ]]; then
+  # null operator because we just built it
+  :
+else 
+  if echo ${AWS} | grep 'Method already exists for this resource' >/dev/null; then
+    :
+  else
+    echo "${AWS}"
+    exit ${EXIT}
+  fi
+fi
+
+
+cat <<EOT > /var/tmp/template.json.$$
+{"application/json": "{     \"StreamName\": \"webhooks\",\n  \"Data\": \"$util.base64Encode($input.path    ('$'))\",\n  \"PartitionKey\": \"shardId-000000000000\" }"}
+EOT
+cat <<EOT > /var/tmp/req_parms.json.$$
+{
+            "integration.request.header.ContentType": "'application/x-amz-json-1.1'"
+        }
+EOT
+# Now add the integration '
+AWS=`aws apigateway put-integration ${IAM_PROFILE} --rest-api-id ${OURAPI} --resource-id ${STREAMSID} --http-method POST --type AWS --integration-http-method POST --uri arn:aws:apigateway:us-west-2:kinesis:action/PutRecord --credentials ${ROLEARN} --request-parameters file:///var/tmp/req_parms.json.$$ --request-templates file:///var/tmp/template.json.$$  2>&1`
+EXIT=$?
+if [[ ${EXIT} -eq 0 ]]; then
+  # null operator because we just built it
+  :
+else 
+  if echo ${AWS} | grep 'Method already exists for this resource' >/dev/null; then
+    :
+  else
+    echo "${AWS}"
+    exit ${EXIT}
+  fi
+fi
+
+# And the integration response, because AWS is fucking craycray
+AWS=`aws apigateway put-integration-response ${IAM_PROFILE} --rest-api-id ${OURAPI} --resource-id ${STREAMSID} --http-method POST --status-code 200 --response-templates '{ "application/json": ""}' 2>&1`;
+EXIT=$?
+if [[ ${EXIT} -eq 0 ]]; then
+  # null operator because we just built it
+  :
+else 
+  if echo ${AWS} | grep 'Method already exists for this resource' >/dev/null; then
+    :
+  else
+    echo "${AWS}"
+    exit ${EXIT}
+  fi
+fi
+
